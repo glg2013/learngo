11a96bc8b01bc9aeafd82a872f3e27713bc3939b
gaofeng <57412960@qq.com>
2025-03-07 18:52:15 +0800

    测试局部变量

--- daily/main.go
+++ daily/main.go
@@ -8,23 +8,32 @@ type userInfo struct {
 }
 
 func main() {
-	fmt.Println("hello world")
-	test(2, 3)
+	//fmt.Println("hello world")
+	//test(2, 3)
+	//
+	//var n = 1
+	//var p *int = &n
+	//fmt.Println(p)
+	//
+	//var m = new(int)
+	//fmt.Println(*m)
+	//
+	//var usr = userInfo{
+	//	name: "hello",
+	//}
+	//fmt.Println("usr.name-> ", usr.name)
+	//// 看看可以修改不
+	//usr.name = "no hello"
+	//fmt.Println("usr.name2-> ", usr.name)
 
-	var n = 1
-	var p *int = &n
-	fmt.Println(p)
-
-	var m = new(int)
-	fmt.Println(*m)
-
-	var usr = userInfo{
-		name: "hello",
+	x := "hello!"
+	for i := 0; i < len(x); i++ {
+		x := x[i]
+		if x != '!' {
+			x := x + 'A' - 'a'
+			fmt.Printf("%c", x)
+		}
 	}
-	fmt.Println("usr.name-> ", usr.name)
-	// 看看可以修改不
-	usr.name = "no hello"
-	fmt.Println("usr.name2-> ", usr.name)
 }
 
 func test(x ...int) {
